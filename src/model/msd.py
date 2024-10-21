fix bug msd lead to gradient zero out

src/model/msd.py
@@ -55,7 +55,7 @@ class MSD(Qwen2VLPreTrainedModel):
         video_grid_thw: Optional[torch.LongTensor] = None,
         rope_deltas: Optional[torch.LongTensor] = None,
     ):
-        seq_logits = self.model(
+        logits = self.model(
             input_ids,
             attention_mask,
             position_ids,
@@ -74,32 +74,50 @@ class MSD(Qwen2VLPreTrainedModel):
 
         for encoder_layer in self.encoder_layers:
             if self.gradient_checkpointing and self.training:
-                seq_logits = self._gradient_checkpointing_func(
+                logits = self._gradient_checkpointing_func(
                     encoder_layer.__call__,
-                    seq_logits,
+                    logits,
                     attention_mask=attention_mask,
                     position_ids=position_ids,
                     past_key_values=past_key_values,
                     output_attentions=output_attentions,
                 )
             else:
-                seq_logits = encoder_layer(
-                    seq_logits,
+                logits = encoder_layer(
+                    logits,
                     attention_mask=attention_mask,
                     position_ids=position_ids,
                     past_key_values=past_key_values,
                     output_attentions=output_attentions,
                 )
 
-        mean_logits = self.masked_mean(seq_logits, attention_mask, 1)
+        if input_ids is not None:
+            batch_size = input_ids.shape[0]
+        else:
+            batch_size = inputs_embeds.shape[0]
+
+        if self.config.pad_token_id is None and batch_size != 1:
+            raise ValueError("Cannot handle batch sizes > 1 if no padding token is defined.")
+        if self.config.pad_token_id is None:
+            sequence_lengths = -1
+        else:
+            if input_ids is not None:
+                # if no pad token found, use modulo instead of reverse indexing for ONNX compatibility
+                sequence_lengths = torch.eq(input_ids, self.config.pad_token_id).int().argmax(-1) - 1
+                sequence_lengths = sequence_lengths % input_ids.shape[-1]
+                sequence_lengths = sequence_lengths.to(logits.device)
+            else:
+                sequence_lengths = -1
+        pooled_logits = logits[torch.arange(batch_size, device=logits.device), sequence_lengths]
+
+        # mean_logits = self.masked_mean(logits, attention_mask, 1)
         if self.gradient_checkpointing and self.training:
             logits = self._gradient_checkpointing_func(
                 self.classification_layer.__call__,
-                mean_logits,
+                pooled_logits,
             )
         else:
-            logits = self.classification_layer(mean_logits)
-
+            logits = self.classification_layer(pooled_logits)
         loss = self.compute_loss(logits, labels)
 
         return dict(loss=loss, logits=logits)
@@ -131,7 +149,6 @@ class MSD(Qwen2VLPreTrainedModel):
     def compute_loss(self, logits, labels):
         loss_fc = CrossEntropyLoss()
         loss = loss_fc(logits, labels)
-
         return loss
 
 

src/train/train.py
@@ -7,6 +7,7 @@ import re
 from sklearn.metrics import f1_score
 
 from torch.utils.data import DataLoader
+
 from qwen_vl_utils import process_vision_info
 from transformers import Qwen2VLProcessor, AutoProcessor
 
@@ -101,6 +102,8 @@ def train(config):
         )
     else:
         processor = AutoProcessor.from_pretrained(model_args.base_model)
+
+    collator = MSDDataCollator(processor, padding=True, max_length=data_args.pop("max_length", None))
     train_data_path = data_args.pop("train_data", None)
     if getattr(data_args, "val_data", None):
         val_data_path = data_args.pop("val_data", None)
@@ -113,8 +116,6 @@ def train(config):
         **train_data_path,
         **data_args,
     )
-
-    collator = MSDDataCollator(processor, padding=True)
 
     ################### Load model
     if model_args.base_model:
@@ -166,6 +167,8 @@ def train(config):
         model.config.use_cache = False
         model.enable_input_require_grads()
 
+    print(model)
+
     if training_args.pop("use_lora", None):
         target_modules = lora_args.pop("target_modules")
         if target_modules == "all-linear":
@@ -178,8 +181,8 @@ def train(config):
             **lora_args
         )
         # print(target_modules)
-        if lora_args.modules_to_save == []:
-            lora_args.pop("modules_to_save", None)
+        # if lora_args.modules_to_save == []:
+        #     lora_args.pop("modules_to_save", None)
         model = get_peft_model(model, lora_config)
 
     # ############################################################ TRAINER